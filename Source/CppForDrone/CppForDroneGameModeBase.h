fd100b26b67d6934db9dab8a12f9b38caa267943
Tack <1422620600@qq.com>
2020-07-29 07:06:15 +0800

    init

---
+++ Source/CppForDrone/CppForDroneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CppForDroneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPFORDRONE_API ACppForDroneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
